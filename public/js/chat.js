3f6ce52445477ff52bd82531983060ffa585527f
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2024-01-18 12:07:01 +0530

    added greeting message and location sharing option on client connection

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,43 @@
 const socket = io();
 
-socket.on("countUpdated", (message) => {
-    console.log(`The count has been updated!. Current Count = ${message}`);
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = document.getElementById("input");
+const $messageFormButton = document.querySelector("button");
+const $locationButton = document.getElementById("send-location-button");
+
+socket.on("greet", (message) => {
+    console.log(`Greetings from server: ${message}`);
 })
 
-document.querySelector("#increment").addEventListener("click", () => {
-    console.log("Clicked!");
-    socket.emit("increment");
+socket.on("server-message", (message) => {
+    console.log(`Received message from server: ${message}`);
 })
+
+$messageFormButton.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    console.log("clicked");
+    $messageFormButton.setAttribute("disabled", "disabled");
+    // disable the form
+    const message = event.target.elements.message.value;
+    socket.emit("client-message", message, (error) => {
+        // enable
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message delivered.");
+    })
+})
+
+$locationButton.addEventListener("click", () => {
+    if (!navigator.geolocation) {
+        return alert('Gelocation is not supported by your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('client-location', {latitude: position.coords.latitude, longitude: position.coords.longitude}, (message) => {
+            console.log(message);
+        })
+    })
+});
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const config = require("config")
 const path = require("path");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 
 const app = express();
@@ -14,20 +15,33 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
 io.on("connection", (socket) => {
    console.log("[*] Client connected!")
-   socket.emit("countUpdated", count);
+   socket.emit("greet", "Welcome!");
+   socket.broadcast.emit("server-message", "A user has joined!")
 
-   socket.on("increment", () => {
-      count = count + 1;
-      // socket.emit("countUpdated", count);
-      io.emit("countUpdated", count);
+   socket.on("client-message", (message, callback) => {
+      const filter = new Filter();
+
+      if (filter.isProfane(message)) {
+         return callback('Profanity is not allowed!');
+      }
+      io.emit("server-message", message);
+      return callback('delivered');
+   })
+
+   socket.on("disconnect", () => {
+      io.emit("server-message", "A user has left!")
+   })
+
+   socket.on("client-location", (coords, callback) => {
+      io.emit("server-message", `Location: https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+      callback("Location is shared with users.");
    })
 });
 
 server.listen(webPort, () => {
-   console.log(`[*] Server running on port http://localhost:${webPort}`)
+   console.log(`[*] Server running on port http://localhost:${webPort}`);
 });
 
 module.exports = app;
